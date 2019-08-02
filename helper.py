94efffac28939e64fc726b2600893fc95ab34b03
belek0 <lipendina.tn@gmail.com>
2019-08-02 22:17:29 +0300

    Добавлен метод для валидации входных данных

--- helper.py
+++ helper.py
@@ -1,10 +1,13 @@
+from collections import defaultdict
+
+
 def tuple_to_dict(tup):
     return {
         'citizen_id': tup[0],
         'town': tup[1],
         'street': tup[2],
         'building': tup[3],
-        'appartement': tup[4],
+        'apartment': tup[4],
         'name': tup[5],
         'birth_date': tup[6],
         'gender': tup[7],
@@ -12,4 +15,31 @@ def tuple_to_dict(tup):
     }
 
 
+def validation(get_data):
+    fields = {
+        'citizen_id': int,
+        'town': str,
+        'street': str,
+        'building': str,
+        'apartment': int,
+        'name': str,
+        'birth_date': str,
+        'gender': str,
+        'relatives': list
+    }
+
+    for i in get_data['citizens']:
+        for key, value in i.items():
+            if key not in fields.keys():
+                return True
+            if type(value) != fields[key]:
+                return True
+    dct = defaultdict(list)
+    for i in get_data['citizens']:
+        dct[i['citizen_id']] = i['relatives']
+
+    for citizen, relatives in dct.items():
+        for relative in relatives:
+            if citizen not in dct[relative]:
+                return True
 
